fc9db7fd2a75f1fd55b1d49035945189d5c05248
Todd Wolfson <todd@twolfson.com>
2014-11-05 04:51:03 -0800

    At a point where we can start hacking

--- test/ecma-variable-scope-test.js
+++ test/ecma-variable-scope-test.js
@@ -4,4 +4,9 @@ var esprima = require('esprima-fb');
 var ecmaVariableScope = require('../');
 
 describe('ecma-variable-scope', function () {
+  it('dev work...', function () {
+    var script = fs.readFileSync(__dirname + '/explore.js');
+    var ast = esprima.parse(script);
+    ecmaVariableScope(ast);
+  });
 });
