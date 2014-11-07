72afedd7a4f5c11769a784537f7fdc23a4d0188f
Todd Wolfson <todd@twolfson.com>
2014-11-07 02:59:39 -0800

    Moving test files inside tests

--- test/top-level.js
+++ test/top-level.js
@@ -1,14 +1,14 @@
 // Load in dependencies
-var fs = require('fs');
 var esprima = require('esprima-fb');
+var fnToString = require('function-to-string');
 var ecmaVariableScope = require('../');
 
 // Define test utilities
 var testUtils = {
-  loadScript: function (filepath) {
+  interpretAst: function (fn) {
     before(function loadScriptFn () {
       // Load the file, parse its AST, and run `ecmaVariableScope` through it
-      var script = fs.readFileSync(filepath);
+      var script = fnToString.body;
       this.ast = esprima.parse(script);
       ecmaVariableScope(this.ast);
     });
@@ -21,6 +21,15 @@ var testUtils = {
 // Run our tests
 describe('ecma-variable-scope', function () {
   describe('marking up an AST with a top level var', function () {
+    testUtils.interpretAst(function topLevelFn () {
+      // Define variable at top level
+      var hello;
+      function hai() {
+        // Reference again out of top level context
+        hello = true;
+      }
+    });
+
     // TODO: Consider putting source inside of test to make testing easier
     it('marks the initialization as top level', function () {
 
@@ -30,4 +39,6 @@ describe('ecma-variable-scope', function () {
 
     });
   });
+
+  // describe('mark
 });
