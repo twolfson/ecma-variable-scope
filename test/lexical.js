77721850e61960b2dbf94757f3856afeaf781421
Todd Wolfson <todd@twolfson.com>
2014-11-07 03:49:56 -0800

    Checking out const status

--- test/lexical.js
+++ test/lexical.js
@@ -9,7 +9,7 @@ describe('ecma-variable-scope', function () {
       var hello = 'world';
     });
 
-    it.only('is considered `lexical`', function () {
+    it('is considered `lexical`', function () {
       // {Program} (body) -> {var} ([0]) -> hello (declarations[0].id)
       var identifier = this.ast.body[0].declarations[0].id;
       expect(identifier.scopeInfo).to.have.property('type', 'lexical');
