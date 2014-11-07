8ac7f23748b2392538d6f391e0ff71c1345704d2
Todd Wolfson <todd@twolfson.com>
2014-11-07 03:53:05 -0800

    Added test against const

--- test/lexical.js
+++ test/lexical.js
@@ -1,3 +1,5 @@
+/* jshint -W104 */
+// DEV: The JSHint directive above ignores `const` usage
 // Load in dependencies
 var expect = require('chai').expect;
 var scriptUtils = require('./utils/script');
@@ -7,6 +9,18 @@ describe('ecma-variable-scope', function () {
   describe('marking up an AST with a top level `var`', function () {
     scriptUtils.interpretFnAst(function () {
       var hello = 'world';
+    });
+
+    it('is considered `lexical`', function () {
+      // {Program} (body) -> {var} ([0]) -> hello (declarations[0].id)
+      var identifier = this.ast.body[0].declarations[0].id;
+      expect(identifier.scopeInfo).to.have.property('type', 'lexical');
+    });
+  });
+
+  describe('marking up an AST with a top level `const`', function () {
+    scriptUtils.interpretFnAst(function () {
+      const hello = 'world';
     });
 
     it('is considered `lexical`', function () {
