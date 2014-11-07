2ac33b69b1fb109dc08d33006be8261ec415bb71
Todd Wolfson <todd@twolfson.com>
2014-11-07 03:46:03 -0800

    Finished up lexical assertion against `var`

--- test/lexical.js
+++ test/lexical.js
@@ -4,16 +4,15 @@ var scriptUtils = require('./utils/script');
 
 // Run our tests
 describe('ecma-variable-scope', function () {
-  describe('marking up an AST with a var inside referenced through a `with`', function () {
-    scriptUtils.interpretFnAst(function topLevelFn () {
+  describe('marking up an AST with a top level `var`', function () {
+    scriptUtils.interpretFnAst(function () {
       var hello = 'world';
-      var obj = {hello: 'moon'};
     });
 
-    it.skip('marks the initialization without a `with`', function () {
+    it.only('is considered `lexical`', function () {
       // {Program} (body) -> {var} ([0]) -> hello (declarations[0].id)
       var identifier = this.ast.body[0].declarations[0].id;
-      expect(identifier.scopeInfo).to.have.property('insideWith', false);
+      expect(identifier.scopeInfo).to.have.property('type', 'lexical');
     });
   });
 });
