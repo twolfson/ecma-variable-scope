5267c7bce8b29af1026062d1870d1e4569f9eefe
Todd Wolfson <todd@twolfson.com>
2014-11-07 04:06:55 -0800

    Starting to work on function items

--- test/lexical.js
+++ test/lexical.js
@@ -6,7 +6,7 @@ var scriptUtils = require('./utils/script');
 
 // Run our tests
 describe('ecma-variable-scope', function () {
-  describe('marking up an AST with a top level `var`', function () {
+  describe('marking up an AST with a `var`', function () {
     scriptUtils.interpretFnAst(function () {
       var hello = 'world';
     });
@@ -18,7 +18,7 @@ describe('ecma-variable-scope', function () {
     });
   });
 
-  describe('marking up an AST with a top level `const`', function () {
+  describe('marking up an AST with a `const`', function () {
     scriptUtils.interpretFnAst(function () {
       const hello = 'world';
     });
@@ -27,6 +27,34 @@ describe('ecma-variable-scope', function () {
       // {Program} (body) -> {var} ([0]) -> hello (declarations[0].id)
       var identifier = this.ast.body[0].declarations[0].id;
       expect(identifier.scopeInfo).to.have.property('type', 'lexical');
+    });
+  });
+
+  describe.only('marking up an AST with a function declaration', function () {
+    scriptUtils.interpretFnAst(function () {
+      function hello(world) {
+        // Code goes here
+      }
+    });
+
+    it.skip('marks the function name as lexically scoped', function () {
+
+    });
+
+    it.skip('marks the function arguments as lexically scoped', function () {
+
+    });
+  });
+
+  describe.skip('marking up an AST with an unnamed function expression', function () {
+    scriptUtils.interpretFnAst(function () {
+      [].map(function (world) {
+        // Code goes here
+      });
+    });
+
+    it('does not cause errors', function () {
+      // Errors would throw in `before`
     });
   });
 });
