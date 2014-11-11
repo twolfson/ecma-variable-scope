ad969829db71f8babe92faf57ef84cdb4c6578ba
Todd Wolfson <todd@twolfson.com>
2014-11-10 22:55:23 -0800

    Adding new utilities

--- test/utils/script.js
+++ test/utils/script.js
@@ -4,12 +4,28 @@ var fnToString = require('function-to-string');
 var ecmaVariableScope = require('../../');
 
 // Define test utilities
+exports._interpretStrAst = function (script) {
+  // Parse the AST and run `ecmaVariableScope`
+  var ast = esprima.parse(script);
+  ecmaVariableScope(ast);
+  return ast;
+};
+
 exports.interpretFnAst = function (fn) {
   before(function loadScriptFn () {
-    // Load the file, parse its AST, and run `ecmaVariableScope` through it
+    // Load the file and interpret it
     var script = fnToString(fn).body;
-    this.ast = esprima.parse(script);
-    ecmaVariableScope(this.ast);
+    this.ast = exports._interpretStrAst(script);
+  });
+  after(function cleanup () {
+    delete this.ast;
+  });
+};
+
+exports.interpretStrAst = function (script) {
+  before(function loadScriptFn () {
+    // Interpret the provided script
+    this.ast = exports._interpretStrAst(script);
   });
   after(function cleanup () {
     delete this.ast;
