94bd4e0c4fe48787a99c2b9264c677919c8e458f
Todd Wolfson <todd@twolfson.com>
2014-11-11 03:26:12 -0800

    Added test for scope type

--- test/scope.js
+++ test/scope.js
@@ -9,38 +9,44 @@ var scriptUtils = require('./utils/script');
 describe('ecma-variable-scope', function () {
   describe.only('marking up an AST for its scopes', function () {
     scriptUtils.interpretFnAst(function () {
-      var logger = {
-        info: function () {}
-      };
-      logger.info('hello');
+      function hello(world) {
+        var hai = true;
+      }
+    });
+    before(function grabScope () {
+      // {Program} (ast) -> {function} (body[0]) -> {var} (body.body[0])
+      //  -> hai (declarations[0].id)
+      this.identifier = this.ast.body[0].body.body[0].declarations[0].id;
+      this.scope = this.identifier.scope;
+    });
+    after(function cleanup () {
+      delete this.identifier;
+      delete this.scope;
     });
 
     it('gives a `scope` a `type`', function () {
+      expect(this.scope).to.have.property('type', 'lexical');
+    });
+
+    it.skip('gives a `scope` a `node`', function () {
       // {Program} (ast) -> {logger.info} (body[1]) -> logger (expression.callee.object)
       var identifier = this.ast.body[1].expression.callee.object;
       expect(identifier).to.have.property('scope');
       expect(identifier).to.have.property('scopeInfo');
     });
 
-    it('gives a `scope` a `node`', function () {
+    it.skip('gives a `scope` a `parent`', function () {
       // {Program} (ast) -> {logger.info} (body[1]) -> logger (expression.callee.object)
       var identifier = this.ast.body[1].expression.callee.object;
       expect(identifier).to.have.property('scope');
       expect(identifier).to.have.property('scopeInfo');
     });
 
-    it('gives a `scope` a `parent`', function () {
-      // {Program} (ast) -> {logger.info} (body[1]) -> logger (expression.callee.object)
-      var identifier = this.ast.body[1].expression.callee.object;
-      expect(identifier).to.have.property('scope');
-      expect(identifier).to.have.property('scopeInfo');
-    });
-
-    it('saves its identifiers', function () {
+    it.skip('saves its identifiers', function () {
 
     });
 
-    it('saves its child scopes', function () {
+    it.skip('saves its child scopes', function () {
 
     });
   });
