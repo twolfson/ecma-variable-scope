1ba61c08d86d15f454d1e8f45741e3e8b9893cb0
Todd Wolfson <todd@twolfson.com>
2014-11-11 02:02:03 -0800

    Taking more notes

--- test/scope.js
+++ test/scope.js
@@ -17,10 +17,12 @@ describe('ecma-variable-scope', function () {
     it.only('does not mark the `info` property with a `scope`', function () {
       // {Program} (ast) -> {logger.info} (body[1]) -> info (expression.callee.property)
       var identifier = this.ast.body[1].expression.callee.property;
-      console.log(this.ast.body[2]);
+      expect(identifier).to.not.have.property('scope');
       expect(identifier).to.not.have.property('scopeInfo');
     });
   });
+
+  // TODO: Pull list from substack's project as well
 
   // Double check all of these
   // label
