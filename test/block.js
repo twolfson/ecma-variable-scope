fe6da14e8d55c0dfd91f54e3051e607727084d13
Todd Wolfson <todd@twolfson.com>
2014-11-11 00:14:57 -0800

    Happily surprised that for/for in/for of are already covered

--- test/block.js
+++ test/block.js
@@ -26,7 +26,7 @@ describe('ecma-variable-scope', function () {
 
 // Intermediate tests
 describe('ecma-variable-scope', function () {
-  describe.only('marking up an AST with a `block` statement', function () {
+  describe('marking up an AST with a `block` statement', function () {
     scriptUtils.interpretFnAst(function () {
       'use strict';
       if (true) {
@@ -57,16 +57,15 @@ describe('ecma-variable-scope', function () {
     });
 
     it('a `block` parameter is considered `block` scoped', function () {
-      // {Program} (ast) -> {if} (body[1]) -> {block} (consequent) -> {let} (body[0])
-      //   -> hello (declarations[0].id)
-      var identifier = this.ast.body[1].consequent.body[0].declarations[0].id;
+      // {Program} (ast) -> {for} (body[1]) -> {let} (init) -> hello (declarations[0].id)
+      var identifier = this.ast.body[1].init.declarations[0].id;
       expect(identifier.scopeInfo).to.have.property('type', 'block');
     });
 
-    it.skip('scopes the variable to the `for` statement', function () {
-      var block = this.ast.body[1].consequent;
-      var identifier = block.body[0].declarations[0].id;
-      expect(identifier.scope.node).to.equal(block);
+    it('scopes the variable to the `for` statement', function () {
+      var forLoop = this.ast.body[1];
+      var identifier = forLoop.init.declarations[0].id;
+      expect(identifier.scope.node).to.equal(forLoop);
     });
   });
 });
