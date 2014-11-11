c856945d4f7b4910dcbd10d649add4394e469d02
Todd Wolfson <todd@twolfson.com>
2014-11-10 23:40:53 -0800

    Patching up issues with `let`

--- test/block.js
+++ test/block.js
@@ -5,21 +5,23 @@ var scriptUtils = require('./utils/script');
 // Run our tests
 // Basic tests
 describe('ecma-variable-scope', function () {
-  describe('marking up an AST with a `let`', function () {
+  describe.only('marking up an AST with a `let`', function () {
     scriptUtils.interpretFnAst(function () {
       'use strict';
       let hello = 'world';
     });
 
     it('is considered `block`', function () {
-      // {Program} (ast) -> {let} (body[0]) -> hello (declarations[0].id)
-      var identifier = this.ast.body[0].declarations[0].id;
+      // {Program} (ast) -> {let} (body[1]) -> hello (declarations[0].id)
+      var identifier = this.ast.body[1].declarations[0].id;
       expect(identifier.scopeInfo).to.have.property('type', 'block');
     });
 
     it('scopes the variable to the top level', function () {
-      var identifier = this.ast.body[0].declarations[0].id;
+      var identifier = this.ast.body[1].declarations[0].id;
       expect(identifier.scope.node).to.equal(this.ast);
     });
   });
+
+  // TODO: Verify `let` stops in a "function"
 });
