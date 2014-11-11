509b724593de25091ed07bfcf9bfeb370ececbd3
Todd Wolfson <todd@twolfson.com>
2014-11-11 00:09:31 -0800

    Adding test for for loops

--- test/block.js
+++ test/block.js
@@ -26,7 +26,7 @@ describe('ecma-variable-scope', function () {
 
 // Intermediate tests
 describe('ecma-variable-scope', function () {
-  describe.only('marking up an AST with a block statement', function () {
+  describe.only('marking up an AST with a `block` statement', function () {
     scriptUtils.interpretFnAst(function () {
       'use strict';
       if (true) {
@@ -41,7 +41,29 @@ describe('ecma-variable-scope', function () {
       expect(identifier.scopeInfo).to.have.property('type', 'block');
     });
 
-    it('scopes the variable to the block statement', function () {
+    it('scopes the variable to the `block` statement', function () {
+      var block = this.ast.body[1].consequent;
+      var identifier = block.body[0].declarations[0].id;
+      expect(identifier.scope.node).to.equal(block);
+    });
+  });
+
+  describe.only('marking up an AST with a for loop', function () {
+    scriptUtils.interpretFnAst(function () {
+      'use strict';
+      for (let hello = 'world'; false; false) {
+        // Code goes here
+      }
+    });
+
+    it('a `block` parameter is considered `block` scoped', function () {
+      // {Program} (ast) -> {if} (body[1]) -> {block} (consequent) -> {let} (body[0])
+      //   -> hello (declarations[0].id)
+      var identifier = this.ast.body[1].consequent.body[0].declarations[0].id;
+      expect(identifier.scopeInfo).to.have.property('type', 'block');
+    });
+
+    it.skip('scopes the variable to the `for` statement', function () {
       var block = this.ast.body[1].consequent;
       var identifier = block.body[0].declarations[0].id;
       expect(identifier.scope.node).to.equal(block);
@@ -66,7 +88,7 @@ describe('ecma-variable-scope', function () {
       expect(identifier.scopeInfo).to.have.property('type', 'block');
     });
 
-    it('scopes the variable to the block statement', function () {
+    it('scopes the variable to the `block` statement', function () {
       var identifier = this.ast.body[1].declarations[0].id;
       expect(identifier.scope.node).to.equal(this.ast);
     });
