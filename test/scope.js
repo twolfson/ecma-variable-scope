3252993fdb548e7d00212583d4e07c68d6cb91a6
Todd Wolfson <todd@twolfson.com>
2014-11-11 02:10:12 -0800

    Fixed another test

--- test/scope.js
+++ test/scope.js
@@ -6,7 +6,7 @@ var scriptUtils = require('./utils/script');
 // Anything with the `Identifier` that isn't a variable reference
 // https://developer.mozilla.org/en-US/docs/Mozilla/Projects/SpiderMonkey/Parser_API
 describe('ecma-variable-scope', function () {
-  describe('marking up an AST with a call expression', function () {
+  describe('marking up an AST with a MemberExpression', function () {
     scriptUtils.interpretFnAst(function () {
       var logger = {
         info: function () {}
@@ -14,7 +14,7 @@ describe('ecma-variable-scope', function () {
       logger.info('hello');
     });
 
-    it.only('does not mark the `info` property with a `scope`', function () {
+    it('does not mark the `info` property with a `scope`', function () {
       // {Program} (ast) -> {logger.info} (body[1]) -> info (expression.callee.property)
       var identifier = this.ast.body[1].expression.callee.property;
       expect(identifier).to.not.have.property('scope');
@@ -22,13 +22,27 @@ describe('ecma-variable-scope', function () {
     });
   });
 
-  // TODO: Pull list from substack's project as well
+  describe('marking up an AST with an object Property', function () {
+    scriptUtils.interpretFnAst(function () {
+      var logger = {
+        info: function () {}
+      };
+    });
+
+    it.only('does not mark the `info` property with a `scope`', function () {
+      // {Program} (ast) -> {var} (body[0]) -> {logger} (declarations[0].init) -> info (properties[0].key)
+      var identifier = this.ast.body[0].declarations[0].init.properties[0].key;
+      expect(identifier).to.not.have.property('scope');
+      expect(identifier).to.not.have.property('scopeInfo');
+    });
+  });
+
+  // TODO: Verify again with ES5 `get`/`set
 
   // Double check all of these
   // label
   // break
   // continue
   // property
-  // member expression
   // objectpattern key
 });
