2501f06ad294eb57e1f0c739f93c1445b30bde97
Todd Wolfson <todd@twolfson.com>
2014-11-11 01:50:27 -0800

    Added tests to cover

--- test/edge-cases.js
+++ test/edge-cases.js
@@ -4,6 +4,8 @@ var scriptUtils = require('./utils/script');
 
 // Run our tests
 // Identifier edge cases
+// Anything with the `Identifier` that isn't a variable referenc
+// https://developer.mozilla.org/en-US/docs/Mozilla/Projects/SpiderMonkey/Parser_API
 describe('ecma-variable-scope', function () {
   describe('marking up an AST with a call expression', function () {
     scriptUtils.interpretFnAst(function () {
@@ -16,7 +18,16 @@ describe('ecma-variable-scope', function () {
     it.only('does not mark the `info` property with a `scope`', function () {
       // {Program} (ast) -> {logger.info} (body[1]) -> info (expression.callee.property)
       var identifier = this.ast.body[1].expression.callee.property;
+      console.log(this.ast.body[2]);
       expect(identifier).to.not.have.property('scopeInfo');
     });
   });
+
+  // Double check all of these
+  // label
+  // break
+  // continue
+  // property
+  // member expression
+  // objectpattern key
 });
